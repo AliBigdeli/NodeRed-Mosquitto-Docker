81d9306a7246d677dc7143b33979ed3a211ca574
TheAliBigdeli <bigdeli.ali3@gmail.com>
2023-09-01 00:26:31 +0330

    first init of the docker-compose

---
+++ dockerfiles/nodered/settings.js
@@ -0,0 +1,15 @@
+adminAuth: {
+    type: "credentials",
+    users: [
+        {
+            username: "admin",
+            password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
+            permissions: "*"
+        },
+        {
+            username: "george",
+            password: "$2b$08$wuAqPiKJlVN27eF5qJp.RuQYuy6ZYONW7a/UWYxDTtwKFCdB8F19y",
+            permissions: "read"
+        }
+    ]
+}
